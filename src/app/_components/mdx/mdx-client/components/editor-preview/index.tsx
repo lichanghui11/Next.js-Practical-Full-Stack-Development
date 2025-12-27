f3f5db8ee0aa31df7afe78696262303b6dc67834
lichanghui <weeesti470@gmail.com>
2025-12-27 18:08:52 +0800

    fix: 提交保存以追踪 AI 对代码的更改

--- src/app/_components/mdx/mdx-client/components/editor-preview/index.tsx
+++ src/app/_components/mdx/mdx-client/components/editor-preview/index.tsx
@@ -3,6 +3,8 @@ import type { FC } from 'react';
 
 import { isNil } from 'lodash';
 
+import { Skeleton } from '@/app/_components/shadcn/ui/skeleton';
+
 import type { EditorPreviewProps } from '../../types';
 
 import { MdxHydration } from '../../mdx-hydration';
@@ -16,10 +18,22 @@ export const EditorPreview: FC<EditorPreviewProps> = ({ serialized, loading = fa
   if (loading) {
     return (
       <div className={styles.previewContainer}>
-        <div className={styles.skeleton}>
-          <div className={styles.skeletonLine} />
-          <div className={styles.skeletonLine} />
-          <div className={styles.skeletonLine} />
+        <div className="flex flex-col space-y-3">
+          <Skeleton className="h-[125px] w-full rounded-xl" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-full" />
+          </div>
+          <Skeleton className="h-[125px] w-full rounded-xl" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-full" />
+          </div>
+          <Skeleton className="h-[125px] w-full rounded-xl" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-full" />
+          </div>
         </div>
       </div>
     );
